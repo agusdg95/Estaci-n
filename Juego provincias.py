c98e4c7f03f365d8b5ec7318047fc82c4da9e09d
agus.damiani@gmail.com <agus.damiani@gmail.com>
2023-03-23 16:45:43 -0300

    Se agrega el proyecto

---
+++ Juego provincias.py
@@ -0,0 +1,131 @@
+print('El juego consiste en indicar la capital exacta de cada provincia. Recuerda respetar las mayúsuculas y las tildes donde corresponda ¿estás preparado?:')
+print('Comencemos por la región pampena.')
+buenosAires = input('Buenos Aires: ')
+if buenosAires == 'La Plata':
+   print('Correcto')
+else: print('Incorrecto, la capital es "La Plata."')
+
+córdoba = input('Córdoba: ')
+if córdoba == 'Córdoba':
+    print('Correcto')
+
+else: print(print('Incorrecto, la capital es "Córdoba."'))
+
+santaFe = input('Santa Fe: ')
+if santaFe == 'Santa Fe':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "Santa Fe."'))
+
+entreRios = input('Entre Rios: ')
+if entreRios == 'Paraná':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "Paraná."'))
+
+laPampa = input('La Pampa: ')
+if laPampa == 'Santa Rosa':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "Santa Rosa."'))
+
+print('Muy bien, sigamos con la Patagonia.')
+
+tierraDelFuego = input('Tierra del Fuego:  ')
+if tierraDelFuego == 'Ushuaia':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "Ushuaia".'))
+
+santaCruz = input('Santa Cruz: ')
+if santaCruz == 'Río Gallegos':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "Río Gallegos".'))
+
+chubut = input('Chubut: ')
+if chubut == 'Rawson':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "Rawson".'))
+
+neuquen = input('Neuquén: ')
+if neuquen == 'Neuquén':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "Neuquén".'))
+
+print('Felicidades, ahora pasemos a la región de Cuyo.')
+
+mendoza = input('Mendoza: ')
+if mendoza == 'Mendonza':
+    print('Correcto')
+else:
+    print('Incorrecto, la capital era "Mendoza".')
+
+sanLuis = input('San Luis: ')
+if sanLuis == 'San Luis':
+    print('Correcto')
+else: print('Incorrecto, la capital es "San Luis".')
+
+sanJuan = input('San Juan: ')
+if sanJuan == 'San Juan':
+    print('Correcto')
+else: print('Incorrecto, la capital es "San Juan".')
+
+laRioja = input('La Rioja: ')
+if laRioja == 'La Rioja':
+    print('Correcto')
+else: print('Incorrecto, la capital es "La Rioja".')
+
+print('Muy bien, viajemos a la región noroeste.')
+
+catamarca = input("Catamarca: ")
+if catamarca == 'San Fernando del Valle de Catamarca':
+    print('Correcto')
+else: print('Incorrecto, la capital es "San Fernando del Valle de Catamarca".')
+
+tucuman = input('Tucumán: ')
+if tucuman == 'San Miguel de Tucumán':
+    print('Correcto')
+else:
+    print(print('Incorrecto, la capital es "San Miguel de Tucumán".'))
+
+santiagoDelEstero = input('Santiago del Estero: ')
+if santiagoDelEstero == 'Santiago del Estero':
+    print('Correcto')
+else: print('Incorrecto, la capital es "Santiago del Estero".')
+
+salta = input('Salta: ')
+if salta == 'Salta':
+    print('Correcto')
+else: print('Incorrecto, la capital es "Salta".')
+
+jujuy = input('Jujuy: ')
+if jujuy == 'San Salvador de Jujuy':
+    print('Correcto')
+else: print('Incorrecto, la capital es "San Salvador de Jujuy".')
+
+print('Finalicemos con el nordeste')
+
+formosa = input('Formosa: ')
+if formosa == 'Formosa':
+    print('Correcto')
+else: print('Incorrecto, la capital es "Formosa".')
+
+chaco = input('Chaco: ')
+if chaco == 'Resistencia':
+    print('Correcto')
+else: print('Incorrecto, la capital es "Resistencia".')
+
+corrientes = input('Corrientes: ')
+if corrientes == 'Corrientes':
+    print('Correcto')
+else: print('Incorrecto, la capital es "Corrientes".')
+
+misiones= input('Misiones: ')
+if misiones == 'Posadas':
+    print('Correcto')
+else: print('Incorrecto, la capital es "Posadas".')
+
+print('Felicidades, has completado todas las capitales :D ')
